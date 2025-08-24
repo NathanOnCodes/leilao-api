8a2ea1f3815147d1c992461d4d748086dab3c1ee
Nathan Cavalcante <contatonathancf@gmail.com>
2025-08-23 21:57:23 -0300

    feat: model Usuarios e migrations

--- produtos/models.py
+++ produtos/models.py
@@ -1,5 +1,5 @@
+import datetime
 from django.db import models
-
 
 class NegociacaoDoProduto(models.TextChoices):
     ABERTO = "ABERTO", "Aberto"
@@ -9,7 +9,16 @@ class NegociacaoDoProduto(models.TextChoices):
 class Produtos(models.Model):
     nome = models.CharField(max_length=100)
     valor_inicial = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.TextField()
     negociacao_do_produto = models.CharField(
         choices=NegociacaoDoProduto.choices, default=NegociacaoDoProduto.ABERTO
     )
     data_leilao = models.DateField()
+    dono_do_produto = models.ForeignKey(
+        Usuarios,
+        on_delete=models.CASCADE,
+        related_name="produtos",
+    )
+
+    def __str__(self):
+        return self.nome
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuarios(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=128)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome']
+
+
+    def __str__(self):
+        return self.nome
