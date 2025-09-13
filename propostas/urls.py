26ccbd5fa2149c6e9f692b966a06e1305494e339
Nathan Cavalcante <contatonathancf@gmail.com>
2025-09-13 16:27:12 -0300

    feat: urls do app propostas

---
+++ propostas/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import SimpleRouter
+from propostas.views import PropostaViewSet
+
+router_propostas = SimpleRouter()
+
+router_propostas.register(r'propostas/', PropostaViewSet, basename='propostas')
