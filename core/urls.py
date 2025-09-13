ce0b284fea874d1259da76d080cd0be12d764e12
Nathan Cavalcante <contatonathancf@gmail.com>
2025-09-13 16:28:25 -0300

    feat: inclusao em urlpatterns de core rotas de api/propostas/

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from usuarios.urls import router_usuarios
+from propostas.urls import router_propostas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router_usuarios.urls)),
+    path('api/', include(router_propostas.urls)),
 ]
